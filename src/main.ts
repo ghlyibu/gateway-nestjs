a6e716ef064c9e7a07ba4144d34c83165acacb53
guohl <guohl@gistack.cn>
2022-08-08 20:14:10 +0800

    feat: 统一响应体格式

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 接口版本化管理
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
