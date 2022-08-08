49a55b58fad21f28cae36282370ca5a22737ae4a
guohl <guohl@gistack.cn>
2022-08-08 21:27:30 +0800

    feat: 环境配置测试

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 import { parse } from 'yaml';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
 
 // 获取项目运行环境
